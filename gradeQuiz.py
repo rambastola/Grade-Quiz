a36e65bd03a27b62894349e0b2adb0f2436b219f
Ram Bastola <rambastola7@gmail.com>
2019-02-26 17:18:10 -0500

    calculates solo and group quiz grades

--- gradeQuiz.py
+++ gradeQuiz.py
@@ -6,18 +6,54 @@ import re
 df = pd.read_excel('/Users/rambastola/Desktop/CSC124.xlsx')
 q1, q2, q3, q4, q5 = df['Question 1'],df['Question 2'],df['Question 3'],df['Question 4'],df['Question 5']
 
-
-def gradeAnswers(student, correctAns, stdAns):
-    print(student, correctAns, stdAns)
-
-def studentAnswers(students, answers, quizNum, user):
+def studentAnswers(students, correctAns, quizNum, user):
     """
-    get student name and quiz answers and student answers
+    students: all the student
+    correctAns: all quizes answers
+    quizNum: all the quizes classification
+    user: int. classification of the quiz to be graded
     """
     for i in range(len(students[0])):
-        if quizNum[i] == answers[user][0]:
+        if quizNum[i] == correctAns[user][0]: #choose the correct set of answers
             student = re.sub('[.]', '', students[0][i]) #removing '.' from some names
-            gradeAnswers(student,answers[user],[q1[i], q2[i], q3[i], q4[i], q5[i]])
+            gradeAnswers(student,correctAns[user],[q1[i], q2[i], q3[i], q4[i], q5[i]])
+
+def gradeAnswers(student, correctAns, stdAns):
+    """
+    student: str of students
+    correctAns: array of correct answers
+    stdAns: Student answers
+    """
+
+    soloPoints = 0
+    groupPoints = 0
+
+    if student.count(',') == 1: #single quiz. up to 10 points
+        if correctAns[1] == stdAns[0]:
+            soloPoints +=2
+        if correctAns[2] == stdAns[1]:
+            soloPoints +=2
+        if correctAns[3] == stdAns[2]:
+            soloPoints +=2
+        if correctAns[4] == stdAns[3]:
+            soloPoints +=2
+        if correctAns[5] == stdAns[4]:
+            soloPoints +=2
+        print(student, soloPoints)
+
+    elif student.count(',') > 1: #group quiz. up to 5 points
+        if correctAns[1] == stdAns[0]:
+            groupPoints +=1
+        if correctAns[2] == stdAns[1]:
+            groupPoints +=1
+        if correctAns[3] == stdAns[2]:
+            groupPoints +=1
+        if correctAns[4] == stdAns[3]:
+            groupPoints +=1
+        if correctAns[5] == stdAns[4]:
+            groupPoints +=1
+        # print(student, groupPoints)
+
 
 def main():
     user = int(input("Which quiz do you want to grade? ")) # which quiz to grade?
@@ -27,14 +63,13 @@ def main():
     quizNum = df['Which quiz are you submitting answers for?'] # all of the quiz
 
     quizAns=[]
-    student = []
-    student.append(names)
+    student = [names]
     students = np.array(student)
     for i in range(len(df.index)):
         if names[i] == "1 Correct Answer" or names[i] == "1 Answer Key":
             quizAns.append([quizNum[i], q1[i], q2[i], q3[i], q4[i], q5[i]])
 
-    answers = np.array(quizAns)  #lists of correct quiz answers
-    studentAnswers(students, answers, quizNum, user)
+    correctAns = np.array(quizAns)  #lists of correct quiz answers
+    studentAnswers(students, correctAns, quizNum, user)
 
 main()
