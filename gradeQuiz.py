8007cae1d7ae30f7364e6ab4515a3dbb4bbadb09
Ram Bastola <rambastola7@gmail.com>
2019-02-27 01:15:24 -0500

    accurately calculate and store grades for students in a dictionary

--- gradeQuiz.py
+++ gradeQuiz.py
@@ -6,10 +6,20 @@ import re
 df = pd.read_excel('/Users/rambastola/Desktop/CSC124.xlsx')
 q1, q2, q3, q4, q5 = df['Question 1'],df['Question 2'],df['Question 3'],df['Question 4'],df['Question 5']
 
-def grade(student, soloPoints):
-    s = []
-    s.append(student)
-    print(s)
+def grade(solo, group):
+    """
+    solo: dict of single student name and their score
+    group: dict of multi-std names and score
+    """
+
+    official = {}
+    for key, value in solo.items():
+        for k, v in group.items():
+            if key in k:
+                gp = group[k]
+                official[key] = gp + solo[key]
+
+    print(official)
 
 
 def studentAnswers(students, correctAns, quizNum, user):
@@ -19,47 +29,47 @@ def studentAnswers(students, correctAns, quizNum, user):
     quizNum: all the quizes classification
     user: int. classification of the quiz to be graded
     """
-    for i in range(len(students[0])):
-        if quizNum[i] == correctAns[user][0]: #choose the correct set of answers
-            student = re.sub('[.]', '', students[0][i]) #removing '.' from some names
-            evalAnswers(student,correctAns[user],[q1[i], q2[i], q3[i], q4[i], q5[i]])
-
-def evalAnswers(student, correctAns, stdAns):
-    """
-    student: str of students
-    correctAns: array of correct answers
-    stdAns: Student answers
-    """
 
     soloPoints = 0
     groupPoints = 0
-    # for name in args:
 
+    solo = {}
+    group= {}
 
-    if student.count(',') == 1: #single quiz. up to 10 points
-        if correctAns[1] == stdAns[0]:
-            soloPoints +=2
-        if correctAns[2] == stdAns[1]:
-            soloPoints +=2
-        if correctAns[3] == stdAns[2]:
-            soloPoints +=2
-        if correctAns[4] == stdAns[3]:
-            soloPoints +=2
-        if correctAns[5] == stdAns[4]:
-            soloPoints +=2
+    for i in range(len(students[0])):
+        if quizNum[i] == correctAns[user][0]: #choose the correct set of answers
+            student = re.sub('[.]', '', students[0][i]) #removing '.' from some names
 
-    elif student.count(',') > 1: #group quiz. up to 5 points
-        if correctAns[1] == stdAns[0]:
-            groupPoints +=1
-        if correctAns[2] == stdAns[1]:
-            groupPoints +=1
-        if correctAns[3] == stdAns[2]:
-            groupPoints +=1
-        if correctAns[4] == stdAns[3]:
-            groupPoints +=1
-        if correctAns[5] == stdAns[4]:
-            groupPoints +=1
-    grade(student, soloPoints, )
+            if student.count(',') == 1: #single quiz. up to 10 points
+                if correctAns[user][1] == q1[i]:
+                    soloPoints +=2
+                if correctAns[user][2] == q2[i]:
+                    soloPoints +=2
+                if correctAns[user][3] == q3[i]:
+                    soloPoints +=2
+                if correctAns[user][4] == q4[i]:
+                    soloPoints +=2
+                if correctAns[user][5] == q5[i]:
+                    soloPoints +=2
+
+                solo[student] =soloPoints
+                soloPoints = 0
+
+            elif student.count(',') > 1: #group quiz. up to 5 points
+                if correctAns[user][1] == q1[i]:
+                    groupPoints +=1
+                if correctAns[user][2] == q2[i]:
+                    groupPoints +=1
+                if correctAns[user][3] == q3[i]:
+                    groupPoints +=1
+                if correctAns[user][4] == q4[i]:
+                    groupPoints +=1
+                if correctAns[user][5] == q5[i]:
+                    groupPoints +=1
+
+                group[student] =groupPoints
+                groupPoints = 0
+    grade(solo, group)
 
 
 def main():
